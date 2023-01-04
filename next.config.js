ce19dd45dce8a6a3dc879fda016870eda796e905
Afopefoluwa Ojo <afopefoluwa.ojo@adidas.com>
2023-01-04 13:33:33 +0100

    feature: commit server side path

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true' || false
+
+const serverSettings = {}
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  serverSettings.basePath = `/${repo}`
+  serverSettings.assetPrefix = `/${repo}/`
+}
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     unoptimized: true,
-    assetPrefix: '/devops-project/',
-    basePath: '/devops-project', // Note that the basePath doesnt end on a slash
-  }
-}
+  },
+  ...serverSettings,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
